339d447d8af97c02b9f207586408a97218840728
Yuan Gao <yuagao@student.ethz.ch>
2024-02-08 15:44:45 +0100

    rchol test code

--- julia-files/matlabSafe.jl
+++ julia-files/matlabSafe.jl
@@ -263,3 +263,69 @@ function timeLimitLamgSddm(limit, la, b; tol::Real=1e-8, maxits=1000, verbose=fa
 
 end
 
+function timeLimitRchol(limit, la, b; tol::Real=1e-8, maxits=1000, verbose=false)
+
+    mf = MatFile("fromJulia.mat","w")
+    put_variable(mf, "la", la)
+    put_variable(mf, "b", b)
+    put_variable(mf, "tol", tol)
+    put_variable(mf, "maxits", maxits)
+    close(mf)
+
+    # lapdir = dirname(pathof(Laplacians))
+    
+    # fn = "$(lapdir)/../matlab/timeIcc.m"
+    fn = string(cd(pwd, ".."), "/", "matlab/timeRchol.m")
+    mat = ENV["MATLAB_HOME"]
+    matlab = "$(mat)/bin/matlab"
+    #cmd = `gtimeout $(limit) $(matlab) -nojvm \< $(fn)`
+    # seems that gtimeout is not recognized?
+    cmd = `timeout $(limit) $(matlab) -nojvm \< $(fn)`
+
+    t0 = now()
+
+    bt = Inf
+    st = Inf
+    err = Inf
+    iter = Inf
+    start = -Inf
+
+    try
+        run(cmd)
+    catch e
+        #rethrow();
+        errtrace = backtrace()
+        msg = sprint(showerror, e, errtrace)
+        println(msg)
+        println("Matlab Died")
+    end
+
+    try
+        mf = MatFile("toJulia.mat")
+
+        bt = get_variable(mf,:bt)
+        st = get_variable(mf,:st)
+        err = get_variable(mf,:relres)
+        iter = get_variable(mf,:iter)
+        start = get_variable(mf,:startTime)
+        close(mf)
+    catch
+        #rethrow()
+        println("No .mat file generated")
+    end
+    
+
+
+    if verbose
+      println("Build Time: ", bt)
+      println("Solve Time: ", st)
+      println("Iterations: ", iter)
+      println("error: ", err)
+      d1 = DateTime(start,"d-u-yyyy H:M:S")
+      println("Time to load and start Matlab: $(d1-DateTime(t0))/1000)")
+    end
+
+
+    return (st, bt, iter, err)
+
+end
---
+++ matlab/timeRchol.m
@@ -0,0 +1,29 @@
+load fromJulia.mat
+
+startTime = datestr(now);
+st = Inf;
+bt = Inf;
+iter = Inf;
+relres = Inf;
+save toJulia st bt iter relres startTime
+
+tic();
+n = length(la)
+p = amd(la);
+laperm = la(p,p);
+L = rchol(laperm);
+
+bt = toc();
+
+tic();
+[xs,flag,relres,iter] = pcg(laperm, b(p), tol, maxits, L, L');
+x = zeros(n,1);
+x(p) = xs;
+st = toc();
+
+relres = norm(la*x-b)/norm(b)
+
+
+save toJulia st bt iter relres startTime
+
+exit
